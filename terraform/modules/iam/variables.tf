56901e39516be06be8d894555cb9fdc5f71b4bb8
Matt Jarrett <cujarrett@gmail.com>
2021-04-08 14:02:22 -0500

    feat: adds new database schemas

--- terraform/modules/iam/variables.tf
+++ terraform/modules/iam/variables.tf
@@ -1,12 +1,12 @@
-# variable "destiny_insights_backend_mods_table_arn" {
-#   description = "DynamoDB destiny_insights_backend_mods table ARN"
-#   type = string
-# }
+variable "destiny_insights_backend_mods_table_arn" {
+  description = "DynamoDB destiny_insights_backend_mods table ARN"
+  type = string
+}
 
-# variable "destiny_insights_backend_xur_table_arn" {
-#   description = "DynamoDB destiny_insights_backend_xur table ARN"
-#   type = string
-# }
+variable "destiny_insights_backend_xur_table_arn" {
+  description = "DynamoDB destiny_insights_backend_xur table ARN"
+  type = string
+}
 
 variable "destiny_insights_backend_bungie_api_auth_table_arn" {
   description = "DynamoDB destiny_insights_backend_bungie_api_auth table ARN"
