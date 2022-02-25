24ebad9715f4cff5eb7b5c60a9bca8d5818e774a
Matt Jarrett <16245634+cujarrett@users.noreply.github.com>
2022-02-25 11:43:25 -0600

    feat: adds support for Xur selling Hawkmoon and Dead Mans Tale (#233)

--- src/util/get-vendor-inventory.js
+++ src/util/get-vendor-inventory.js
@@ -183,17 +183,16 @@ module.exports.getVendorInventory = async (vendorHash) => {
           if (vendorWeaponPerks[key] && Object.keys(vendorWeaponPerks[key].plugs).length > 2) {
             perk1 = vendorWeaponPerks[key].plugs["1"][0].plugItemHash
             perk2 = vendorWeaponPerks[key].plugs["1"][1].plugItemHash
+            // Swords perk locations are weird :(
             // eslint-disable-next-line max-len
             perk3 = vendorWeaponPerks[key].plugs["1"]?.[2]?.plugItemHash || vendorWeaponPerks[key].plugs["2"][0].plugItemHash
-            // Swords perk locations are weird :(
             // eslint-disable-next-line max-len
             perk4 = vendorWeaponSockets[key].sockets?.[2]?.plugHash || vendorWeaponPerks[key].plugs["2"][1].plugItemHash
             perk5 = vendorWeaponPerks[key].plugs["3"][0].plugItemHash
             perk6 = vendorWeaponPerks[key].plugs["4"][0].plugItemHash
           }
         } else {
-          // eslint-disable-next-line max-len
-          if (vendorWeaponPerks[key] && Object.keys(vendorWeaponPerks[key].plugs).length > 2) {
+          if (vendorWeaponPerks[key] && !weapon.type.startsWith("Exotic")) {
             perk1 = vendorWeaponPerks[key].plugs["1"][0].plugItemHash
             perk2 = vendorWeaponPerks[key].plugs["1"][1].plugItemHash
             perk3 = vendorWeaponPerks[key].plugs["2"][0].plugItemHash
