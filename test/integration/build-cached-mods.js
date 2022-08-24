1aecf2d185655d6cb1d175889df874a0f2c167d0
Matt jarrett <cujarrett@gmail.com>
2022-08-24 11:33:51 -0600

    fix: updates cache

--- test/integration/build-cached-mods.js
+++ test/integration/build-cached-mods.js
@@ -8,5 +8,5 @@ test("Integration - buildCachedMods", async (assert) => {
   assert.is(type, "Legendary Weapon Mod")
   assert.is(description, "Increases duration of Rampage.")
   // eslint-disable-next-line max-len
-  assert.is(icon, "https://bungie.net/common/destiny2_content/icons/b3eadd1fcadf34e389fad3e7a75acc26.jpg")
+  assert.is(icon, "https://bungie.net/common/destiny2_content/icons/8fb859ce78bf1fbc2c677fa013d3961e.png")
 })
