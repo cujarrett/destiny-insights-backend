071487164971de31b9a3ae3210026d0754d7813b
Matt jarrett <cujarrett@gmail.com>
2022-12-18 23:13:06 -0600

    fix: updates dependencies

--- test/integration/devrim-kay.js
+++ test/integration/devrim-kay.js
@@ -4,6 +4,6 @@ const { getVendorInventory } = require("../../src/util/get-vendor-inventory.js")
 test("Integration - devrim-kay Vendor", async (assert) => {
   const vendorInventory = await getVendorInventory("396892126")
   assert.truthy(vendorInventory.inventory.armor.length > 0)
-  assert.truthy(vendorInventory.inventory.weapons.length > 0)
+  assert.truthy(vendorInventory.inventory.weapons.length === 0)
   assert.truthy(vendorInventory.inventory.mods.length === 0)
 })
