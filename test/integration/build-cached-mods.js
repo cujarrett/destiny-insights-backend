4ca8773e9ab98f5681d66ae18147a15de8d55cc3
Matt Jarrett <16245634+cujarrett@users.noreply.github.com>
2022-12-18 23:14:33 -0600

    fix: updates dependencies (#315)

--- test/integration/build-cached-mods.js
+++ test/integration/build-cached-mods.js
@@ -3,10 +3,10 @@ const { buildCachedMods } = require("../../src/util/build-cached-mods.js")
 
 test("Integration - buildCachedMods", async (assert) => {
   const cachedMods = await buildCachedMods()
-  const { name, type, description, icon } = cachedMods["666440382"]
-  assert.is(name, "Rampage Spec")
-  assert.is(type, "Legendary Weapon Mod")
-  assert.is(description, "Increases duration of Rampage.")
+  const { name, type, description, icon } = cachedMods["3185435910"]
+  assert.is(name, "Charged Up")
+  assert.is(type, "Common Charged with Light Mod")
+  assert.is(description, "Allows for 1 additional stack of Charged with Light.")
   // eslint-disable-next-line max-len
-  assert.is(icon, "https://bungie.net/common/destiny2_content/icons/8fb859ce78bf1fbc2c677fa013d3961e.png")
+  assert.is(icon, "https://bungie.net/common/destiny2_content/icons/ce29f32ba5ef8758d1b5f3cfad0944a6.png")
 })
--- test/integration/failsafe.js
+++ test/integration/failsafe.js
@@ -4,6 +4,6 @@ const { getVendorInventory } = require("../../src/util/get-vendor-inventory.js")
 test("Integration - failsafe Vendor", async (assert) => {
   const vendorInventory = await getVendorInventory("1576276905")
   assert.truthy(vendorInventory.inventory.armor.length > 0)
-  assert.truthy(vendorInventory.inventory.weapons.length > 0)
+  assert.truthy(vendorInventory.inventory.weapons.length === 0)
   assert.truthy(vendorInventory.inventory.mods.length === 0)
 })
