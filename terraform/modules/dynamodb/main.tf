3cd143a71444c48e2b0b30afd6547e708454e65e
Matt Jarrett <16245634+cujarrett@users.noreply.github.com>
2023-01-08 19:54:52 -0600

    feat: adds consumer auth table (#316)
    * feat: adds consumer auth table

--- terraform/modules/dynamodb/main.tf
+++ terraform/modules/dynamodb/main.tf
@@ -18,3 +18,24 @@ resource "aws_dynamodb_table" "destiny_insights_backend_bungie_api_auth" {
 output "destiny_insights_backend_bungie_api_auth_arn" {
   value = aws_dynamodb_table.destiny_insights_backend_bungie_api_auth.arn
 }
+
+resource "aws_dynamodb_table" "destiny_insights_backend_bungie_consumer_auth" {
+  name           = "destiny-insights-backend-bungie-consumer-auth"
+  billing_mode   = "PROVISIONED"
+  read_capacity  = 1
+  write_capacity = 1
+  hash_key       = "id"
+
+  attribute {
+    name = "id"
+    type = "S"
+  }
+
+  lifecycle {
+    prevent_destroy = true
+  }
+}
+
+output "destiny_insights_backend_bungie_consumer_auth_arn" {
+  value = aws_dynamodb_table.destiny_insights_backend_bungie_consumer_auth.arn
+}
